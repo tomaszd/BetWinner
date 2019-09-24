eca0c8dc3b93de7ea1afe7885b8554da7214dd7b
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2019-09-24 12:27:40 +0200

    added results obtainerinfo

--- DataDownloader/ResultsCollector.py
+++ DataDownloader/ResultsCollector.py
@@ -1,3 +1,6 @@
+import json
+
+import os
 import requests
 from bs4 import BeautifulSoup
 
@@ -46,6 +49,7 @@ class ResultsParser():
                 win=single_team_data[4].text,
                 loose=single_team_data[5].text,
                 goals=single_team_data[6].text,
+                last_matches=[]
             )
             self.teams.append(single_team_dict)
 
@@ -55,15 +59,46 @@ class ResultsParser():
             team_result_dict = {}
             for j, single_result in enumerate(single_team_results.find_all("td")[2:]):
                 if single_result.text == '    ':
-                    team_result_dict[only_teams_names[j]]="N/A"
+                    team_result_dict[only_teams_names[j]] = "N/A"
                 else:
                     team_result_dict[only_teams_names[j]] = single_result.text.strip()
             pass
             self.teams[i]["results"] = team_result_dict
-        pass
+        series = []
+
+        for single_serie in self.raw_data.find_all("table", {"class": "main", "align": "center"}):
+            serie = []
+            if "Kolejka" in single_serie.text:
+                continue
+
+            for single_match in single_serie.find_all("tr"):
+                rows = single_match.find_all("td")
+                if len(rows) != 4:
+                    continue
+                home_team, result, away_team, date = [x.text.strip() for x in rows]
+                match = {"home": home_team,
+                         "away": away_team,
+                         "result": result,
+                         "date": date}
+
+                self.add_last_result(match)
+
+                serie.append(match)
+            if result == "-":
+                break
+            series.append(serie)
 
     def getTeams(self):
         return self.teams
+
+    def add_last_result(self, match):
+        for team in self.teams:
+            if team["name"] in [match["home"], match["away"]]:
+                team["last_matches"].append(match)
+    def save_dat_into_json(self):
+        with open(os.path.join("data",'data.txt'), 'w') as outfile:
+            json.dump(self.teams, outfile)
+
 
 
 if __name__ == "__main__":
@@ -75,3 +110,4 @@ if __name__ == "__main__":
     print("Sytuacja w tabeli: ")
     for i, team in enumerate(parser.teams):
         print(i + 1, " ", team)
+    parser.save_dat_into_json()
