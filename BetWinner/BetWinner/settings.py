621aee3e9e9500df4c1d9c4ebde9aac80b606199
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2019-09-24 16:19:44 +0200

    add files for heroku

--- BetWinner/BetWinner/settings.py
+++ BetWinner/BetWinner/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import django_heroku
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -112,3 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+django_heroku.settings(locals())
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Betwinner.wsgi --log-file -
